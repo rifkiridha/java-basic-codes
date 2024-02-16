30d7c12c54340a265b137f795e976348d9bfe9cc
Rifki Ridha <rifkiridha@gmail.com>
2024-02-16 15:05:24 +0700

    added Pascal's triangle

--- PascalsTriangle.java
+++ PascalsTriangle.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class PascalTriangle {
+public class PascalsTriangle {
 
     public static int factorial(int n) {
         int result = 1;
@@ -23,7 +23,7 @@ public class PascalTriangle {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Pascal Triangle's Application");
+        System.out.println("Pascal's Triangle's Application");
         System.out.println("Input number of lines (max 12):");
         int b = sc.nextInt();
         if (b <= 12) {
