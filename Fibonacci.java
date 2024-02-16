7e4d8980a4816afa8d5c80fcb1f84fea2cc4a5ce
Rifki Ridha <rifkiridha@gmail.com>
2024-02-16 11:36:36 +0700

    added Fibonacci

---
+++ Fibonacci.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+// Simple Java codes to output Fibonacci numbers with if else and for loop
+
+public class Fibonacci {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if (!(n >= 1)) {
+            System.out.println("Input tidak valid");
+        } else {
+            int a1 = 1, a2 = 1, a3;
+            for (int i = 0; i < n; i++) {
+                if (i == 0) {
+                    System.out.println("Deret ke-1 : " + a1);
+                } else if (i == 1) {
+                    System.out.println("Deret ke-2 : " + a2);
+                } else {
+                    a3 = a1 + a2;
+                    System.out.println("Deret ke-" + (i + 1) + " : " + a3);
+                    a1 = a2;
+                    a2 = a3;
+                }
+            }
+        }
+    }
+}
